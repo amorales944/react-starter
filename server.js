1dce7639f40b7852c23c7726a6900a601176dd51
Armando Morales <amorales944@gmail.com>
2019-08-16 11:06:04 -0500

    Added app.get in server

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV === 'production') {
 
   app.use(express.static('client/build'));
-
+  
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
   app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
