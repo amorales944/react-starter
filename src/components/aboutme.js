9acae7ff5e827934e825c7f8c8ce8d986753635c
Armando Morales <amorales944@gmail.com>
2019-08-16 14:03:12 -0500

    About me content filler

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,12 +1,49 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
+class Aboutme extends Component {
+  render() {
+    return (
+      <div >
+        <Grid className="aboutMe-grid">
+        
+          <Cell col={12}>
+          
+            <h5>
+            <h4 style={{marginTop:'0px'}}>About Me</h4>
+            <hr style={{borderTop:'3px solid #e22947'}} />
+                Shank pig bresaola, chicken porchetta tail strip steak ribeye
+              ground round venison boudin. Shank pig sausage alcatra ham hock
+              pancetta cupim turkey spare ribs ground round pork doner pork loin
+              andouille. Jowl pig buffalo meatloaf, pork loin ham shankle turkey
+              boudin bacon andouille kielbasa picanha. Ball tip pancetta bacon
+              jerky, pork loin beef ribs shoulder kevin turducken rump. Cow
+              burgdoggen rump, pig doner flank alcatra ball tip shankle beef
+              ribs leberkas. Frankfurter alcatra fatback biltong short loin
+              filet mignon. Tenderloin jowl tail ham. Biltong pork chop swine
+              short loin jerky shank cow. Leberkas strip steak prosciutto pork.
 
-class Aboutme extends Component  {
-    render()    {
-        return(
-            <div><h1>About me Page</h1></div>
-        )
-    }
+                kielbasa cow shank venison pork chop ham porchetta. Chicken
+              brisket prosciutto tenderloin. Meatball bacon jowl burgdoggen
+              drumstick, porchetta pork belly andouille kevin ball tip bresaola
+              meatloaf doner. Tongue pastrami flank t-bone, fatback landjaeger
+              meatball cupim prosciutto doner. Pork tri-tip spare ribs ham short
+              ribs kevin, burgdoggen flank jerky ham hock chicken rump turkey
+              landjaeger filet mignon. Salami pork kielbasa ham hock jerky
+              meatloaf capicola filet mignon pork loin. Tri-tip sirloin flank
+              brisket picanha swine. Pancetta turkey filet mignon leberkas beef
+              ribs, turducken picanha chicken burgdoggen hamburger shoulder rump
+              buffalo. Pork loin tri-tip drumstick bacon filet mignon biltong
+              pork chop. Salami tail meatball frankfurter pastrami rump swine
+              bacon ground round cupim landjaeger pork turducken.
+              <hr style={{borderTop:'3px solid #e22947'}} />
+            </h5>
+          </Cell>
+          
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default Aboutme;
