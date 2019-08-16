53cb29aaa99bb3dc4b847c703fa89aad9e3e14bd
Armando Morales <amorales944@gmail.com>
2019-08-16 12:16:12 -0500

    corrected font links locations

---
+++ build/precache-manifest.293c09357be937b83967fab2257ff3df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d3135ac39c22c75c1644005b0c4c827",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84bc3a6901eb1c1b8ded",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "7eef4ad9f7c11876f4fd",
+    "url": "/static/css/main.045955b6.chunk.css"
+  },
+  {
+    "revision": "84bc3a6901eb1c1b8ded",
+    "url": "/static/js/2.894a804c.chunk.js"
+  },
+  {
+    "revision": "7eef4ad9f7c11876f4fd",
+    "url": "/static/js/main.21253233.chunk.js"
+  },
+  {
+    "revision": "3e0c70126bd1f9ea8c62",
+    "url": "/static/js/runtime~main.61ad3112.js"
+  }
+]);
